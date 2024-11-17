9fa06df518aff13b2ba23079d6f583400d7f3783
Stefko1997 <blackstefan1997@gmail.com>
2024-11-17 13:38:19 +0100

    Navigation todo

---
+++ src/assets/features/Navigation/Navigation.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import "./navigation.style.css";
+import LogoImg from "../../image.png";
+
+const Navigation = () => {
+  return (
+    <div className="todolist_navigation">
+      <img src={LogoImg} />
+      <div className="navigation_links">
+        <Link to={{ pathname: "/" }}>Home</Link>
+        <Link to={{ pathname: "/task" }}>Task</Link>
+        <Link to={{ pathname: "/tasklist" }}>Task List</Link>
+        <Link to={{ pathname: "/about" }}>About</Link>
+      </div>
+    </div>
+  );
+};
+export default Navigation;
